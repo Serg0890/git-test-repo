c74c8c8758227af350c318fd4d12929cfaf56668
Sergei Starnikov <serg0890@mail.ru>
2023-02-26 03:09:37 +0700

    test github one?

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String name = "Sergey";
         int ears = 32;
+        String birthear = "27.08.1990";
         System.out.println(name + " " + ears);
     }
 }
